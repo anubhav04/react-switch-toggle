af520b445c3d55a5219890954beeecc2c28c8088
Anubhav Chaturvedi <anubhav@designstring.com>
2018-12-04 09:43:28 +0530

    performance update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './styles/switch.scss';
 const ReactSwitch =(props) => {
   return(
-      <div className={props.className ? props.className : "switch-container"} style={props.style}>
+      <div className={props.className ? props.className : "switch-container"} onClick={() => props.onChange() } style={props.style}>
           <label>
-              <input ref="switch" checked={ props.isChecked } onChange={ props.onChange } className="switch" type="checkbox" />
+              <input ref="switch" checked={ props.isChecked } className="switch" type="checkbox" />
               <div>
                   <span><i className="icon icon-toolbar grid-view"></i></span>
                   <span><i className="icon icon-toolbar ticket-view"></i></span>
